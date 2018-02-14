b548a196843cf36ff21f1c52e2efe7191caab87c
davis009 <davis6247@gmail.com>
2018-02-14 16:02:19 +0300

    Solved

--- src/index.js
+++ src/index.js
@@ -1,3 +1,36 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports =  function check(str, bracketsConfig) {  //
   // your solution
+  if(str.length == 0 || str.length == 1) return false;
+
+  var buffer = [];
+
+    for(var  i = 0; i < str.length; i++){       //iterate over char in str
+      for(var y = 0; y < bracketsConfig.length; y++){   //iterate over brackets in bracketsConfig
+         //finding current kind of brackets
+        if(str[i] == bracketsConfig[y][0] || str[i] == bracketsConfig[y][1])  { 
+          if(bracketsConfig[y][0] == bracketsConfig[y][1]){ //special case for the same brackets
+            if(buffer[buffer.length - 1] == bracketsConfig[y][0]){
+              buffer.pop();
+              break;
+            } 
+            buffer.push(str[i]);
+            break;
+          }
+          
+           if(str[i] == bracketsConfig[y][0]) { //push open bracket in the stack
+            buffer.push(str[i])   
+            break;  
+              
+          }         
+          if(buffer[buffer.length - 1] != bracketsConfig[y][0] || buffer.length == 0)
+              return false;
+
+          buffer.pop();  //pop closed bracket
+          break;                
+        }      
+    }
+  }
+    if(buffer.length == 0) return true;
+    
+    return false;
 }
